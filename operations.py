5edf1ff667f12d437177689733ef8d317ec4a03f
varshaverse <varshaverse247@gmail.com>
2022-06-20 14:06:01 -0700

    drills

---
+++ operations.py
@@ -0,0 +1,32 @@
+#Operations
+#Python uses *, /, % then +, & - when performing calculations
+
+print("I'll count my chickens")
+
+print("Hens", 25 + 30 / 6) # this will print 30.0 as a float since it has not been cast to a specific data type
+print("Roosters", 100 - 25 * 3 % 4)
+print("\n")
+
+print("Now I will count the eggs:")
+
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) 
+
+print("\n")
+print("Is it true that 3 + 2 < 5 - 7?")
+
+
+print(3 + 2 < 5 - 7) #returns false
+
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("\n")
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2) # returns a boolean value, True
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
