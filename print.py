a44eba1df206186e3efb8bb8b26a3656ce3bd7ef
varshaverse <varshaverse247@gmail.com>
2022-06-20 14:04:30 -0700

    drills

---
+++ print.py
@@ -0,0 +1,14 @@
+#Printing & Commenting
+
+#the "hashtag" represents a octothorpe used as commenting
+
+print("Hello world") # prints hello world
+print("What would you like to do today?")
+print("Ok, thinking...")
+print("I'm awesome at coding.") 
+print("\n")
+
+print("Ba, Ba, Ba, Black sheep")
+print("Have you had any{} ".format ('wool')) # for short strings use ' ' and long strings use double quotes (" ")
+print("yes, yes, sir, 3 bags full") 
+
