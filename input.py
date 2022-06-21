905e779ae691e85433e620e18080ea94ab1147c0
varshaverse <varshaverse247@gmail.com>
2022-06-21 12:36:23 -0700

    input & scripts

---
+++ input.py
@@ -0,0 +1,15 @@
+# How Input works
+
+print("How old are you?", end=' ') # pc asks how old are you, input is whatever from the keyboard
+age = int(input()) # keyboard data is saved in variable in age
+print("How tall are you?", end=' ')
+height = (input())
+print("How much do you weigh?", end=' ')
+weight = int(input()) # we mention int so we can only get values that are integers not anything else
+
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.") #prints out all your data
+
+age = input("How old are you?")
+print(f"Your age is {age} years old. ")
+
