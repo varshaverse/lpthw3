afbe59de27cf1a3781ab61dd966321fb85260696
varshaverse <varshaverse247@gmail.com>
2022-06-21 17:19:46 -0700

    test

---
+++ testprint.py
@@ -0,0 +1,18 @@
+a  = input("Enter variable a: ")
+b = input("Enter variable b: ")
+c = a  # reassigned a to variable c
+a = b
+b = c
+
+
+
+print(a)
+print(b)
+
+name = input("What is your name? ")
+print('My name is Varsha')
+print("my name is varsha")
+
+print('"my name is varsha"') 
+
+print('"my name is \n varsha"') 
